243374ffd3ab73b5ddcd00b976687c4e86e2cd2f
ibraimabdullaev <135114077+ibraimabdullaev@users.noreply.github.com>
2023-07-05 16:50:22 +0300

    Update app.py

--- hello_app/app.py
+++ hello_app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return f'My server IP address is {os.environ[194.67.103.244]}'
+    return f'My server IP address is {os.environ['194.67.103.244']}'
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
